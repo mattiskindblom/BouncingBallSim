48c1f5e5fa804b73429aab2956eb85fb4e1241b0
Mattis Kindblom <mattis.kindblom@elev.ga.ntig.se>
2021-08-26 13:37:20 +0200

    Försökte hitta upp hur man använder timers etc. (aldrig gjort det förut vad jag kan komma ihåg)

---
+++ src/BouncingBallSim/Main.java
@@ -0,0 +1,16 @@
+package BouncingBallSim;
+
+
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        int ball = 4;
+        double ballacceleration = 9.8;
+
+
+        System.currentTimeMillis();
+
+    }
+}
